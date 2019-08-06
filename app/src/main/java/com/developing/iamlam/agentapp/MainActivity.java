e0cf4fdeb5422d814ad2d6afd3e7aaa3e48936cc
IamLAM <ingeniero@luismonge.com.mx>
2019-08-06 00:44:30 -0700

    Declaring variables for each editText

--- app/src/main/java/com/developing/iamlam/agentapp/MainActivity.java
+++ app/src/main/java/com/developing/iamlam/agentapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button regButton=findViewById(R.id.register_button);
+        EditText nameEdit=findViewById(R.id.name);
+        EditText lastNameEdit=findViewById(R.id.last_name);
+        EditText ageEdit=findViewById(R.id.age);
+        EditText cityEdit=findViewById(R.id.city);
+        EditText streetEdit=findViewById(R.id.street);
+
+
+
+
+
+
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
